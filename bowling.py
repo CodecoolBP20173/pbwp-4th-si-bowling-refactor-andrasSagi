4fc214a2df666f8299507da19768ae5e13ccc598
andrasSagi <andras.sagi8128@gmail.com>
2017-11-26 11:54:20 +0100

    Compose separate functions to decide if a turn was a strike or a spare

--- bowling.py
+++ bowling.py
@@ -2,21 +2,21 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    last_turn = 0
     for turn in range(len(game)):
         this_turn = game[turn]
-        if this_turn == '/':
+        if is_spare(this_turn):
             result += 10 - last_turn
         else:
             result += get_value(this_turn)
         if frame < 10 and get_value(this_turn) == 10:
             next_turn = game[turn+1]
             result += get_value(next_turn)
-            if this_turn.upper() == 'X':
-                if game[turn+2] == '/':
+            if is_strike(this_turn):
+                second_next_turn = game[turn+2]
+                if is_spare(second_next_turn):
                     result += 10 - get_value(next_turn)
                 else:
-                    result += get_value(game[turn+2])
+                    result += get_value(second_next_turn)
         last_turn = get_value(this_turn)
         if not in_first_half:
             frame += 1
@@ -24,7 +24,7 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
-        if this_turn.upper() == 'X':
+        if is_strike(this_turn):
             in_first_half = True
             frame += 1
     return result
@@ -48,3 +48,13 @@ def get_value(char):
         return int(char)
     else:
         raise ValueError()
+
+
+def is_strike(turn):
+    if turn.upper() == 'X':
+        return True
+
+
+def is_spare(turn):
+    if turn == '/':
+        return True
